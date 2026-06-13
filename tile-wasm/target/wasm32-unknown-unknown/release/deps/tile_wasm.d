539965271df1b8d7716154275b663e3c71e09fe6
Masashi Katsumata <wf9a5m75@gmail.com>
2026-06-14 02:50:12 +0900

    chore: remove tile-wasm Rust crate and build artifacts

--- tile-wasm/target/wasm32-unknown-unknown/release/deps/tile_wasm.d
+++
@@ -1,5 +0,0 @@
-/Users/masashi/android-sdk/android-sdk-core/tile-wasm/target/wasm32-unknown-unknown/release/deps/tile_wasm.d: src/lib.rs
-
-/Users/masashi/android-sdk/android-sdk-core/tile-wasm/target/wasm32-unknown-unknown/release/deps/tile_wasm.wasm: src/lib.rs
-
-src/lib.rs:
